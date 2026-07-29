eae05980429f84bb6ef9e02ea980306950334a44
Ze You <chinzeyou@gmail.com>
2026-07-29 13:49:58 +0800

    strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,22 +6,28 @@
 /*   By: zchin <zchin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 13:10:40 by zchin             #+#    #+#             */
-/*   Updated: 2026/07/29 13:18:41 by zchin            ###   ########.fr       */
+/*   Updated: 2026/07/29 13:49:41 by zchin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *strchr(const char *s, int c)
+#include <stdlib.h>
+
+char *ft_strchr(const char *s, int c)
 {
-    unsigned char *str = s;
-    while (*str != '\0')
+	char *str = (char *)s;
+	while (*str != '\0')
+	{
+    	if ((char)c == *str)
+		{
+			return (str);
+		}
+		str++;
+	}
+	if ((char)c == '\0')
     {
-        if (c = *str)
-        {
-            return (str);
-        }
-        str++;
+        return (str);
     }
-    return (str);
+	return (NULL);
 }
 
 #include <string.h>
@@ -29,8 +35,9 @@ char *strchr(const char *s, int c)
 
 int main()
 {
-    char s1[] = "Hello";
-    char s2[] = "Hello";
+	char s1[] = "Hello";
+	char s2[] = "Hello";
 
-    printf("%s\n", strchr(s1, 123));
+	printf("%s\n", strchr(s1, 'e'));
+	printf("%s\n", ft_strchr(s2, 101));
 }
