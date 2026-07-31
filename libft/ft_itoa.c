a2a6eae03cad400df508fdb3f9350527924b1572
Ze You <chinzeyou@gmail.com>
2026-07-31 14:03:54 +0800

    done ft_itoa.c

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -1,4 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zchin <zchin@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/31 13:22:07 by zchin             #+#    #+#             */
+/*   Updated: 2026/07/31 14:03:24 by zchin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
+
+size_t	count_len(int n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n < 0)
+	{
+		n = n * -1;
+		i++;
+	}
+	while (n != 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
+
+int	is_negative(int *n)
+{
+	if (*n < 0)
+	{
+		*n = *n * -1;
+		return (1);
+	}
+	return (0);
+}
 
 char	*ft_itoa(int n)
 {
@@ -7,22 +47,30 @@ char	*ft_itoa(int n)
 	int		is_neg;
 	char	*ans;
 
-	len = 0;
-	is_neg = 0;
-	if (n < 0)
-		len++;
-		is_neg = 1;
-		n = n * -1;
-	while (n > 10)
-		len++;
+	len = count_len(n);
 	ans = malloc(len * sizeof(char));
 	if (ans == NULL)
 		return (NULL);
 	i = 0;
-	if (is_neg)
+	is_neg = is_negative(&n);
+	while (n != 0)
 	{
-		ans[i] = '-';
+		if (is_neg == 1)
+		{
+			ans[i] = '-';
+			is_neg = 0;
+		}
+		ans[len - i - 1] = n % 10 + '0';
+		n /= 10;
 		i++;
 	}
-		
+	ans[len] = '\0';
+	return (ans);
+}
+
+int	main()
+{
+	int n = -123123;
+	printf("%d\n", n);
+	printf("%s", ft_itoa(n));
 }
